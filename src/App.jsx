9138dd65f851fac8788ec2262e9ef210d281845c
Cleve-keem <belloharyourmidey@gmail.com>
2024-12-10 07:49:16 -0800

    uploading

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [bill, setBill] = useState("");
+  const [rate1, setRate1] = useState(0);
+  const [rate2, setRate2] = useState(0);
+
+  const tip = bill * ((rate1 + rate2) / 2) / 100
+
+  function handleClear() {
+    setBill("");
+    setRate1(0);
+    setRate2(0);
+  }
+
   return (
-    <>
-      <Bill />
-      <ServiceRate>How did you like the service?</ServiceRate>
-      <ServiceRate>How did your friend like the service?</ServiceRate>
-    </>
+    <div style={{fontFamily: "sans-serif"}}>
+      <Bill bill={bill} onBill={setBill} />
+      <ServiceRate rate={rate1} onRate={setRate1}>
+        How did you like the service?
+      </ServiceRate>
+      <ServiceRate rate={rate2} onRate={setRate2}>
+        How did your friend like the service?
+      </ServiceRate>
+
+      {bill > 0 && (
+        <div>
+          <h1>
+            You pay ${bill + tip} (${bill} + ${tip} tip)
+          </h1>
+          <button style={{marginTop: 10 }} onClick={handleClear} >Reset</button>
+        </div>
+      )}
+    </div>
   );
 }
 
-function Bill() {
-  const [bill, setBill] = useState(null);
+function Bill({ bill, onBill }) {
   return (
     <div>
       <span>How much the bill?</span>
@@ -20,16 +43,14 @@ function Bill() {
         type="text"
         value={bill}
         onChange={(e) => {
-          setBill(e.target.value);
+          onBill(+e.target.value);
         }}
       />
     </div>
   );
 }
 
-function ServiceRate({ children }) {
-  const [rate, setRate] = useState("dissatisfied");
-
+function ServiceRate({ children, rate, onRate }) {
   return (
     <div>
       <span>{children}</span>
@@ -37,13 +58,13 @@ function ServiceRate({ children }) {
         name="rate"
         value={rate}
         onChange={(e) => {
-          setRate(e.target.value);
+          onRate(Number(e.target.value));
         }}
       >
-        <option value="dissatisfied">Dissatisfied (0%)</option>
-        <option value="okay">It was okay (5%)</option>
-        <option value="good">It was good (10%)</option>
-        <option value="amazing">Absolutely Amazing (20%)</option>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely Amazing (20%)</option>
       </select>
     </div>
   );
